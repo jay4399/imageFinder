f818d1db18df5ea38a38ce6a77aa155e12a28a25
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-06 15:17:01 +0400

    Updates

---
+++ precache-manifest.4fcb6e04ebe3ee5016b5db903436c5e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc1292a41d2786ff3a03d11517f8c6f",
+    "url": "/imageFinder/index.html"
+  },
+  {
+    "revision": "a21d1f77861620d1e6ce",
+    "url": "/imageFinder/static/css/main.cd37b57d.chunk.css"
+  },
+  {
+    "revision": "aff895bc6511c0240c65",
+    "url": "/imageFinder/static/js/2.1d84965e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imageFinder/static/js/2.1d84965e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21d1f77861620d1e6ce",
+    "url": "/imageFinder/static/js/main.4f00af2f.chunk.js"
+  },
+  {
+    "revision": "ac5f75c0c46ad8d109bf",
+    "url": "/imageFinder/static/js/runtime-main.9711a20e.js"
+  }
+]);
